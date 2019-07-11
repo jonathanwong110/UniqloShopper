6bb2545ed2afeaa564d9d524c40c049304186eb6
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-07-11 02:25:01 -0400

    Update shirt.rb

--- uniqloshopper/lib/uniqloshopper/shirt.rb
+++ uniqloshopper/lib/uniqloshopper/shirt.rb
@@ -14,5 +14,27 @@ class UniqloShopper::Shirt
   def self.all
     @@all
   end
+
+  def self.find_shirts(keyword=nil)
+    if keyword == nil
+      return @@all
+    end
+    @@all.find_all { |shirt| shirt.name.upcase.include?(keyword.upcase) }
+  end
   
+  def self.find_if_keyword(keyword)
+    listed = @@all.find { |shirt| shirt.name.upcase.include?(keyword.upcase) }
+    return listed
+  end
+
 end
+
+
+#  def self.find_shirts(keyword)
+#    @@all.find_all do |shirt|
+#      if shirt.name.include?(keyword)
+#        puts shirt.name, "------"
+#      end
+#      shirt.name.include?(keyword)
+#    end
+#  end
