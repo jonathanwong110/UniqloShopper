d06cfd7d7b69505ead68f9ae0adb753ba8b788c6
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-07-14 22:54:17 -0400

    Update shirt.rb

--- uniqloshopper/lib/uniqloshopper/shirt.rb
+++ uniqloshopper/lib/uniqloshopper/shirt.rb
@@ -3,7 +3,8 @@ class UniqloShopper::Shirt
   attr_accessor :name, :price, :url
   
   @@all = []
-  
+  @@keyword = nil
+
   def initialize(name:, price:, url:)
     @name = name
     @price = price
@@ -14,27 +15,36 @@ class UniqloShopper::Shirt
   def self.all
     @@all
   end
+  
+  def self.keyword
+    @@keyword
+  end
+  
+  def self.reset_keyword
+    @@keyword = nil
+  end
+  
+  def self.print_shirts
+    self.find_shirts.each.with_index do |shirt, i|
+      puts "#{i+1}. #{shirt.name}"
+    end
+  end
+  
+  def self.selection(input)
+    return self.find_shirts[input]
+  end
 
-  def self.find_shirts(keyword=nil)
-    if keyword == nil
+  def self.find_shirts
+    if @@keyword == nil
       return @@all
     end
-    @@all.find_all { |shirt| shirt.name.upcase.include?(keyword.upcase) }
+    @@all.find_all { |shirt| shirt.name.upcase.include?(@@keyword.upcase) }
   end
   
   def self.find_if_keyword(keyword)
-    listed = @@all.find { |shirt| shirt.name.upcase.include?(keyword.upcase) }
+    @@keyword = keyword
+    listed = @@all.find { |shirt| shirt.name.upcase.include?(@@keyword.upcase) }
     return listed
   end
 
 end
-
-
-#  def self.find_shirts(keyword)
-#    @@all.find_all do |shirt|
-#      if shirt.name.include?(keyword)
-#        puts shirt.name, "------"
-#      end
-#      shirt.name.include?(keyword)
-#    end
-#  end
