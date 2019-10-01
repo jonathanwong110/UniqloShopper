2f8d7515c25343ff526d3e12bd2fb8aefca34d55
jonathanwong110 <45864420+jonathanwong110@users.noreply.github.com>
2019-10-01 15:39:36 -0400

    Update shirt.rb

--- uniqloshopper/lib/uniqloshopper/shirt.rb
+++ uniqloshopper/lib/uniqloshopper/shirt.rb
@@ -3,7 +3,6 @@ class UniqloShopper::Shirt
   attr_accessor :name, :price, :url
   
   @@all = []
-  @@keyword = nil
 
   def initialize(name:, price:, url:)
     @name = name
@@ -16,35 +15,21 @@ class UniqloShopper::Shirt
     @@all
   end
   
-  def self.keyword
-    @@keyword
-  end
-  
-  def self.reset_keyword
-    @@keyword = nil
-  end
-  
-  def self.print_shirts
-    self.find_shirts.each.with_index do |shirt, i|
-      puts "#{i+1}. #{shirt.name}"
-    end
-  end
-  
-  def self.selection(input)
-    return self.find_shirts[input]
+  def keyword
+    @keyword
   end
 
-  def self.find_shirts
-    if @@keyword == nil
+  def self.find_shirts(keyword=nil)
+    if keyword == nil
       return @@all
     end
-    @@all.find_all { |shirt| shirt.name.upcase.include?(@@keyword.upcase) }
+    @@all.find_all { |shirt| shirt.name.upcase.include?(keyword.upcase) }
   end
   
   def self.find_if_keyword(keyword)
-    @@keyword = keyword
-    listed = @@all.find { |shirt| shirt.name.upcase.include?(@@keyword.upcase) }
+    listed = @@all.find { |shirt| shirt.name.upcase.include?(keyword.upcase) }
     return listed
   end
+
 
 end
